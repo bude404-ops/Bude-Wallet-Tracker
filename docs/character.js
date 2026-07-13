f83d92e37763eb5f864f268bc62fd6c29a3afa9f
BudE Builder <actions@github.com>
2026-07-13 23:25:45 +0000

    BudE Automated Update

--- docs/character.js
+++ docs/character.js
@@ -1,7 +1,12 @@
 function create(){
+
 let idea=document.getElementById('idea').value;
 let style=document.getElementById('style').value;
+
 let result=createCharacter(idea,style);
+
 localStorage.setItem('bude_character',JSON.stringify(result));
+
 document.getElementById('output').textContent=JSON.stringify(result,null,2);
+
 }
